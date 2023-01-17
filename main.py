02a9bcc746c9ae4f357cb58ce580026e69874e7b
metodj <jazbec.metod@gmail.com>
2023-01-17 16:46:22 +0100

    logZ

--- main.py
+++ main.py
@@ -251,6 +251,7 @@ def train(train_loader, model, criterion, optimizer, epoch, num_classes, likelih
 
         if (alpha is not None) and alpha != 0.:
             prod_loss = get_prod_loss(output, criterion, num_classes, T)
+            prod_loss = torch.log(prod_loss + 1e-10)
 
             losses_individual.update(loss.item(), input.size(0))
             losses_prod.update(prod_loss.item(), input.size(0))
