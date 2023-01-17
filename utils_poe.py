cc31ca0923deab011ca9d4af75c2c3275bf83ce7
metodj <jazbec.metod@gmail.com>
2023-01-17 21:44:02 +0100

    log numerics

--- utils_poe.py
+++ utils_poe.py
@@ -5,8 +5,8 @@ from typing import List
 def schedule_T(step, n_steps, T_start=2., T_end=16.):
     return T_start + (T_end - T_start) * torch.sigmoid(-torch.tensor((-step + n_steps/2) / (n_steps/10)))
 
-
-def get_prod_loss(output: List[torch.Tensor], criterion: torch.nn.modules.loss, num_classes: int, T: float) -> torch.Tensor:
+# TODO: fix numerical issues with logZ
+def get_prod_loss(output: List[torch.Tensor], criterion: torch.nn.modules.loss, num_classes: int, T: float, eps: float = 1e-14) -> torch.Tensor:
     """
     Computes logZ part of PoE loss
     """
@@ -22,5 +22,4 @@ def get_prod_loss(output: List[torch.Tensor], criterion: torch.nn.modules.loss, 
         prod_loss += torch.exp(c_loss.clone())
 
     criterion.reduction = 'mean'
-
-    return torch.sum(torch.log(prod_loss))
+    return torch.sum(torch.log(prod_loss + eps))
