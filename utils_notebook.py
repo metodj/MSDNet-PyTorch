0ece2741dd0413f6cdb0d73660365ac12e7ddf88
metodj <jazbec.metod@gmail.com>
2023-02-09 17:41:05 +0100

    paper plots start, decrasin probs not only for consecutive early exits

--- utils_notebook.py
+++ utils_notebook.py
@@ -1,6 +1,6 @@
 import torch
 import numpy as np
-from typing import Dict
+from typing import Dict, Optional, List
 import scipy
 from tqdm import tqdm
 from dataloader import get_dataloaders
@@ -13,16 +13,36 @@ import torchvision.datasets as datasets
 
 # TODO: most of the functions below have an ugly implementation with for loops, vectorize them
 
-def modal_probs_decreasing(_preds: Dict[int, torch.Tensor], _probs: torch.Tensor, layer: int, verbose: bool = False, N: int = 10000) -> Dict[float, float]:
+
+def modal_probs_decreasing(
+    _preds: Dict[int, torch.Tensor],
+    _probs: torch.Tensor,
+    layer: Optional[int] = None,
+    verbose: bool = False,
+    N: int = 10000,
+    diffs_type: str = "consecutive",
+    thresholds: List[float] = [-0.01, -0.05, -0.1, -0.2, -0.5],
+) -> Dict[float, float]:
     """
     nr. of decreasing modal probability vectors in anytime-prediction regime
+
+    function can also be used for grount truth probabilities, set layer=None
     """
-    nr_non_decreasing = {-0.01: 0, -0.05: 0, -0.1: 0, -0.2: 0, -0.5: 0}
-    diffs = []
+    nr_non_decreasing = {threshold: 0 for threshold in thresholds}
+    # diffs = []
     for i in range(N):
-        probs_i = _probs[:, i, _preds[layer - 1][i]].cpu().numpy()
-        diffs_i = np.diff(probs_i)
-        diffs.append(diffs_i.min())
+        if layer is None:
+            c = _preds[i]
+        else:
+            c = _preds[layer - 1][i]
+        probs_i = _probs[:, i, c].cpu().numpy()
+        if diffs_type == "consecutive":
+            diffs_i = np.diff(probs_i)
+        elif diffs_type == "all":
+            diffs_i = probs_decrease(probs_i)
+        else:
+            raise ValueError()
+        # diffs.append(diffs_i.min())
         for threshold in nr_non_decreasing.keys():
             if np.all(diffs_i >= threshold):
                 nr_non_decreasing[threshold] += 1
@@ -31,11 +51,15 @@ def modal_probs_decreasing(_preds: Dict[int, torch.Tensor], _probs: torch.Tensor
                     print(i, probs_i)
     # print(nr_non_decreasing)
     # print(np.mean(diffs))
-    nr_decreasing = {-1. * k: ((N - v) / N) * 100 for k, v in nr_non_decreasing.items()}
+    nr_decreasing = {
+        -1.0 * k: ((N - v) / N) * 100 for k, v in nr_non_decreasing.items()
+    }
     return nr_decreasing
 
 
-def modal_probs_average(_preds: Dict[int, torch.Tensor], _probs: torch.Tensor, layer: int, N: int) -> Dict[float, float]:
+def modal_probs_average(
+    _preds: Dict[int, torch.Tensor], _probs: torch.Tensor, layer: int, N: int
+) -> Dict[float, float]:
     """
     average modal probability in anytime-prediction regime
     """
@@ -72,7 +96,9 @@ def f_probs_ovr_poe(tensor: torch.Tensor) -> torch.tensor:
     return tensor
 
 
-def f_probs_ovr_poe_break_ties(logits, probs_ovr_poe, T=1., softmax=False, sigmoid=False):
+def f_probs_ovr_poe_break_ties(
+    logits, probs_ovr_poe, T=1.0, softmax=False, sigmoid=False
+):
     logits, probs_ovr_poe = torch.clone(logits), torch.clone(probs_ovr_poe)
     # TODO: implement without for loops
     assert len(logits.shape) == 3  # (L, N_test, C)
@@ -83,18 +109,18 @@ def f_probs_ovr_poe_break_ties(logits, probs_ovr_poe, T=1., softmax=False, sigmo
         for n in tqdm(range(logits.shape[1])):
             ovr_mask = probs_ovr_poe[l, n, :] > 0
             preds_n = []
-            sum_n = 0.
+            sum_n = 0.0
             for c in range(logits.shape[2]):
                 if ovr_mask[c]:
                     if sigmoid:
-                        sigmoid_prod = torch.sigmoid(T * logits[:l + 1, n, c]).prod()
+                        sigmoid_prod = torch.sigmoid(T * logits[: l + 1, n, c]).prod()
                     else:
-                        sigmoid_prod = (T * logits[:l + 1, n, c]).prod()
+                        sigmoid_prod = (T * logits[: l + 1, n, c]).prod()
                 else:
-                    sigmoid_prod = 0.
+                    sigmoid_prod = 0.0
                 sum_n += sigmoid_prod
                 preds_n.append(sigmoid_prod)
-            if sum_n > 0.:
+            if sum_n > 0.0:
                 if softmax:
                     preds_n = list(scipy.special.softmax(preds_n))
                 else:
@@ -103,19 +129,21 @@ def f_probs_ovr_poe_break_ties(logits, probs_ovr_poe, T=1., softmax=False, sigmo
         preds.append(preds_l)
     return torch.tensor(preds)
 
+
 # TODO: unit test checking that f_probs_ovr_poe_logits_weighted is the same as f_probs_ovr_poe_break_ties
 
-def f_probs_ovr_poe_logits_weighted(logits, threshold=0.):
+
+def f_probs_ovr_poe_logits_weighted(logits, threshold=0.0):
     C = logits.shape[-1]
     probs = logits.numpy().copy()
-    probs[probs < threshold] = 0.
+    probs[probs < threshold] = 0.0
     probs = np.cumprod(probs, axis=0)
     # normalize
-    probs = (probs / np.repeat(probs.sum(axis=2)[:, :, np.newaxis], C, axis=2))
+    probs = probs / np.repeat(probs.sum(axis=2)[:, :, np.newaxis], C, axis=2)
     return probs
 
 
-def f_probs_ovr_break_ties(logits, probs_ovr, T=1.):
+def f_probs_ovr_break_ties(logits, probs_ovr, T=1.0):
     logits, probs_ovr = torch.clone(logits), torch.clone(probs_ovr)
     # TODO: implement without for loops
     assert len(logits.shape) == 3  # (L, N_test, C)
@@ -126,15 +154,15 @@ def f_probs_ovr_break_ties(logits, probs_ovr, T=1.):
         for n in range(logits.shape[1]):
             ovr_mask = probs_ovr[l, n, :] > 0
             preds_n = []
-            sum_n = 0.
+            sum_n = 0.0
             for c in range(logits.shape[2]):
                 if ovr_mask[c]:
-                    sigmoid_prod = (T * logits[l, n, c])
+                    sigmoid_prod = T * logits[l, n, c]
                 else:
-                    sigmoid_prod = 0.
+                    sigmoid_prod = 0.0
                 sum_n += sigmoid_prod
                 preds_n.append(sigmoid_prod)
-            if sum_n > 0.:
+            if sum_n > 0.0:
                 preds_n = [x / sum_n for x in preds_n]
             preds_l.append(preds_n)
         preds.append(preds_l)
@@ -144,13 +172,15 @@ def f_probs_ovr_break_ties(logits, probs_ovr, T=1.):
 def f_probs_ovr_logits_weighted(logits):
     C = logits.shape[-1]
     probs = logits.numpy().copy()
-    probs[probs < 0] = 0.
+    probs[probs < 0] = 0.0
     # normalize
-    probs = (probs / np.repeat(probs.sum(axis=2)[:, :, np.newaxis], C, axis=2))
+    probs = probs / np.repeat(probs.sum(axis=2)[:, :, np.newaxis], C, axis=2)
     return probs
 
 
-def f_preds_ovr_fallback_ood(probs: torch.Tensor, logits: torch.Tensor, prod: bool = False) -> torch.Tensor:
+def f_preds_ovr_fallback_ood(
+    probs: torch.Tensor, logits: torch.Tensor, prod: bool = False
+) -> torch.Tensor:
     """
     Predictions for OVR likelihood that fallback to logits when probabilities collapse to 0 (OOD)
     """
@@ -158,11 +188,13 @@ def f_preds_ovr_fallback_ood(probs: torch.Tensor, logits: torch.Tensor, prod: bo
     for l in range(probs.shape[0]):
         preds_l = []
         for n in range(probs.shape[1]):
-            if probs[l, n, :].sum() > 0.:
+            if probs[l, n, :].sum() > 0.0:
                 preds_l.append(torch.argmax(probs[l, n, :]))
             else:
                 if prod:
-                    preds_l_n = torch.argmax(torch.sigmoid(logits[:, n, :]).cumprod(dim=0)[l, :])
+                    preds_l_n = torch.argmax(
+                        torch.sigmoid(logits[:, n, :]).cumprod(dim=0)[l, :]
+                    )
                 else:
                     preds_l_n = torch.argmax(logits[l, n, :])
                 preds_l.append(preds_l_n)
@@ -170,7 +202,9 @@ def f_preds_ovr_fallback_ood(probs: torch.Tensor, logits: torch.Tensor, prod: bo
     return torch.tensor(preds)
 
 
-def f_probs_ovr_fallback_ood(probs: torch.Tensor, logits: torch.Tensor, prod: bool = False) -> torch.Tensor:
+def f_probs_ovr_fallback_ood(
+    probs: torch.Tensor, logits: torch.Tensor, prod: bool = False
+) -> torch.Tensor:
     """
     Probabilities for OVR likelihood that fallback to logits when probabilities collapse to 0 (OOD)
     """
@@ -178,7 +212,7 @@ def f_probs_ovr_fallback_ood(probs: torch.Tensor, logits: torch.Tensor, prod: bo
     for l in range(probs.shape[0]):
         preds_l = []
         for n in range(probs.shape[1]):
-            if probs[l, n, :].sum() > 0.:
+            if probs[l, n, :].sum() > 0.0:
                 preds_l.append(probs[l, n, :])
             else:
                 if prod:
@@ -189,7 +223,7 @@ def f_probs_ovr_fallback_ood(probs: torch.Tensor, logits: torch.Tensor, prod: bo
                 preds_l.append(preds_l_n)
         preds.append(torch.stack(preds_l, dim=0))
     return torch.stack(preds, dim=0)
-    
+
 
 def get_ood_ovr(_probs: torch.Tensor, L: int = 7) -> Dict:
     ood_dict_ids = {l: [] for l in range(L)}
@@ -201,47 +235,49 @@ def get_ood_ovr(_probs: torch.Tensor, L: int = 7) -> Dict:
     return ood_dict_ids
 
 
-def f_probs_ovr_poe_logits_weighted_generalized(logits, threshold=0., weights=None):
+def f_probs_ovr_poe_logits_weighted_generalized(logits, threshold=0.0, weights=None):
     L, C = logits.shape[0], logits.shape[-1]
     probs = logits.numpy().copy()
-    probs[probs < threshold] = 0.
+    probs[probs < threshold] = 0.0
     if weights is not None:
         assert logits.shape[0] == weights.shape[0]
         for l in range(L):
             probs[l, :, :] = probs[l, :, :] ** weights[l]
     probs = np.cumprod(probs, axis=0)
     # normalize
-    probs = (probs / np.repeat(probs.sum(axis=2)[:, :, np.newaxis], C, axis=2))
+    probs = probs / np.repeat(probs.sum(axis=2)[:, :, np.newaxis], C, axis=2)
     return probs
 
 
 def get_logits_targets(dataset, model_folder, likelihood, epoch):
-    assert dataset in ['cifar10', 'cifar100']
+    assert dataset in ["cifar10", "cifar100"]
     ARGS = parse_args()
-    ARGS.data_root = 'data'
+    ARGS.data_root = "data"
     ARGS.data = dataset
-    ARGS.save= f'/home/metod/Desktop/PhD/year1/PoE/MSDNet-PyTorch/models/{model_folder}'
-    ARGS.arch = 'msdnet'
+    ARGS.save = (
+        f"/home/metod/Desktop/PhD/year1/PoE/MSDNet-PyTorch/models/{model_folder}"
+    )
+    ARGS.arch = "msdnet"
     ARGS.batch_size = 64
     ARGS.epochs = 300
     ARGS.nBlocks = 7
-    ARGS.stepmode = 'even'
+    ARGS.stepmode = "even"
     ARGS.base = 4
     ARGS.nChannels = 16
     ARGS.j = 16
-    ARGS.num_classes = 100 if dataset == 'cifar100' else 10
+    ARGS.num_classes = 100 if dataset == "cifar100" else 10
     ARGS.step = 2
     ARGS.use_valid = True
-    ARGS.splits = ['train', 'val', 'test']
+    ARGS.splits = ["train", "val", "test"]
     ARGS.likelihood = likelihood
 
     # load pre-trained model
     model = MSDNet(args=ARGS)
-    model_path = f'models/{model_folder}/save_models/checkpoint_{epoch}.pth.tar'
+    model_path = f"models/{model_folder}/save_models/checkpoint_{epoch}.pth.tar"
     state = torch.load(model_path)
     params = OrderedDict()
-    for params_name, params_val in state['state_dict'].items():
-        params[params_name.replace('module.', '')] = params_val
+    for params_name, params_val in state["state_dict"].items():
+        params[params_name.replace("module.", "")] = params_val
         # state['state_dict'][params_name.replace('module.', '')] = state['state_dict'].pop(params_name)
     model.load_state_dict(params)
     model = model.cuda()
@@ -277,14 +313,14 @@ def get_logits_targets_image_net(step=4):
     assert step in [4, 7]
     if step == 4:
         ARGS = parse_args()
-        ARGS.data_root = 'data'
-        ARGS.data = 'ImageNet'
-        ARGS.save= f'/home/metod/Desktop/PhD/year1/PoE/MSDNet-PyTorch/image_net'
-        ARGS.arch = 'msdnet'
+        ARGS.data_root = "data"
+        ARGS.data = "ImageNet"
+        ARGS.save = f"/home/metod/Desktop/PhD/year1/PoE/MSDNet-PyTorch/image_net"
+        ARGS.arch = "msdnet"
         ARGS.batch_size = 64
         ARGS.epochs = 90
         ARGS.nBlocks = 5
-        ARGS.stepmode = 'even'
+        ARGS.stepmode = "even"
         ARGS.base = 4
         ARGS.nChannels = 32
         ARGS.growthRate = 16
@@ -294,20 +330,20 @@ def get_logits_targets_image_net(step=4):
         ARGS.num_classes = 1000
         ARGS.step = 4
         ARGS.use_valid = True
-        ARGS.splits = ['train', 'val', 'test']
-        ARGS.likelihood = 'softmax'
+        ARGS.splits = ["train", "val", "test"]
+        ARGS.likelihood = "softmax"
         ARGS.nScales = len(ARGS.grFactor)
-        MODEL_PATH = f'image_net/msdnet-step=4-block=5.pth.tar'
+        MODEL_PATH = f"image_net/msdnet-step=4-block=5.pth.tar"
     elif step == 7:
         ARGS = parse_args()
-        ARGS.data_root = 'data'
-        ARGS.data = 'ImageNet'
-        ARGS.save= f'/home/metod/Desktop/PhD/year1/PoE/MSDNet-PyTorch/image_net'
-        ARGS.arch = 'msdnet'
+        ARGS.data_root = "data"
+        ARGS.data = "ImageNet"
+        ARGS.save = f"/home/metod/Desktop/PhD/year1/PoE/MSDNet-PyTorch/image_net"
+        ARGS.arch = "msdnet"
         ARGS.batch_size = 64
         ARGS.epochs = 90
         ARGS.nBlocks = 5
-        ARGS.stepmode = 'even'
+        ARGS.stepmode = "even"
         ARGS.base = 7
         ARGS.nChannels = 32
         ARGS.growthRate = 16
@@ -317,19 +353,19 @@ def get_logits_targets_image_net(step=4):
         ARGS.num_classes = 1000
         ARGS.step = 7
         ARGS.use_valid = True
-        ARGS.splits = ['train', 'val', 'test']
-        ARGS.likelihood = 'softmax'
+        ARGS.splits = ["train", "val", "test"]
+        ARGS.likelihood = "softmax"
         ARGS.nScales = len(ARGS.grFactor)
-        MODEL_PATH = f'image_net/msdnet-step=7-block=5.pth.tar'
+        MODEL_PATH = f"image_net/msdnet-step=7-block=5.pth.tar"
 
-    DATA_PATH = 'data/image_net/valid/'
+    DATA_PATH = "data/image_net/valid/"
 
     # load pre-trained model
     model = MSDNet(args=ARGS)
     state = torch.load(MODEL_PATH)
     params = OrderedDict()
-    for params_name, params_val in state['state_dict'].items():
-        params[params_name.replace('module.', '')] = params_val
+    for params_name, params_val in state["state_dict"].items():
+        params[params_name.replace("module.", "")] = params_val
         # state['state_dict'][params_name.replace('module.', '')] = state['state_dict'].pop(params_name)
     model.load_state_dict(params)
     model = model.cuda()
@@ -337,23 +373,31 @@ def get_logits_targets_image_net(step=4):
 
     model_parameters = filter(lambda p: p.requires_grad, model.parameters())
     params = sum([np.prod(p.size()) for p in model_parameters])
-    print(f'nr. of trainable params: {params}')
+    print(f"nr. of trainable params: {params}")
 
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225])
+    normalize = transforms.Normalize(
+        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+    )
 
-    val_set = datasets.ImageFolder(DATA_PATH, transforms.Compose([
+    val_set = datasets.ImageFolder(
+        DATA_PATH,
+        transforms.Compose(
+            [
                 transforms.Resize(256),
                 transforms.CenterCrop(224),
                 transforms.ToTensor(),
-                normalize
-            ]))
+                normalize,
+            ]
+        ),
+    )
 
     val_loader = torch.utils.data.DataLoader(
-                val_set,
-                batch_size=ARGS.batch_size, shuffle=False,
-                num_workers=ARGS.workers, pin_memory=True)
-
+        val_set,
+        batch_size=ARGS.batch_size,
+        shuffle=False,
+        num_workers=ARGS.workers,
+        pin_memory=True,
+    )
 
     logits = []
     targets = []
@@ -379,17 +423,29 @@ def get_logits_targets_image_net(step=4):
 
 
 def get_scale_probs(probs_name_arr, probs_arr, T_arr, targets, C, L):
-
     def scale_probs(_probs, _targets, T):
-        probs_scaled = _probs ** T
-        probs_scaled = probs_scaled / np.repeat(probs_scaled.sum(axis=2)[:, :, np.newaxis], C, axis=2)
+        probs_scaled = _probs**T
+        probs_scaled = probs_scaled / np.repeat(
+            probs_scaled.sum(axis=2)[:, :, np.newaxis], C, axis=2
+        )
         preds_scaled = {i: torch.argmax(probs_scaled, dim=2)[i, :] for i in range(L)}
-        acc_scaled = [(_targets == preds_scaled[i]).sum() / len(_targets) for i in range(L)]
+        acc_scaled = [
+            (_targets == preds_scaled[i]).sum() / len(_targets) for i in range(L)
+        ]
         return probs_scaled, preds_scaled, acc_scaled
 
     scaled_dict = {x: {} for x in probs_name_arr}
     for probs_name, probs_arr in zip(probs_name_arr, probs_arr):
         for T in T_arr:
             scaled_dict[probs_name][T] = scale_probs(probs_arr, targets, T)
-    
+
     return scaled_dict
+
+
+def probs_decrease(probs: np.array) -> np.array:
+    L = len(probs)
+    diffs = []
+    for i in range(L):
+        for j in range(i + 1, L):
+            diffs.append(probs[j] - probs[i])
+    return np.array(diffs)
