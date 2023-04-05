08b1243a2eeaf61af46fbf1dd18939fd061991de
JamesAllingham <james.allingham@gmail.com>
2023-04-05 23:25:38 +0100

    Remove dead code

--- utils_uncertainty.py
+++ utils_uncertainty.py
@@ -1,23 +1,8 @@
 import numpy as np
-import scipy
-import sklearn
 import torch
+import sklearn
+import scipy
 
-# define function to caluculate the Calibration AUC of a classifier given the predicitons, targets, and confidences
-def cal_auc_np(preds, targets, confs):
-    # determine whether the predictions are correct
-    correct = (preds == targets).astype(float)
-
-    # now use built in methods to calculate the AUROC of a binary classifier
-    fpr, tpr, _ = sklearn.metrics.roc_curve(correct, confs)
-    auc = sklearn.metrics.auc(fpr, tpr)
-    return auc
-
-
-
-# define function to calculate the Oracle Collaboration Accuracy of a classifier given the predictions, targets, confidences
-# and the fraction of examples to defer to the oracle. That is, the accuracy of the classifier when defering some examples
-# to the oracle, based on the confidence of the classifier
 def oracle_collab_acc_np(preds, targets, confs, oracle_frac, diff=True):
     # determine whether the predictions are correct
     correct = (preds == targets).astype(float)
@@ -47,33 +32,6 @@ def oracle_collab_acc_np(preds, targets, confs, oracle_frac, diff=True):
     else:
         return overall_acc
 
-
-# define the function to calculate the Oracle Collaboration AUC of a classifier given the predictions, targets, confidences
-# and the fraction of examples to defer to the oracle. That is, the AUC of the classifier when defering some examples
-# to the oracle, based on the confidence of the classifier
-def oracle_collab_auc_np(preds, targets, confs, oracle_frac):
-    # print(preds)
-    # print(targets)
-    # print(confs)
-    # determine whether the predictions are correct
-    correct = (preds == targets).astype(float)
-    # print(correct)
-
-    # get the number of examples to defer to the oracle
-    oracle_num = int(oracle_frac * len(correct))
-    # print(oracle_num)
-
-    # get the examples to defer to the oracle, taking the ones with the lowest confidence
-    oracle_ids = np.argsort(confs.max(axis=-1))[:oracle_num]
-    # print(oracle_ids)
-
-    correct[oracle_ids] = 1.
-
-    fpr, tpr, _ = sklearn.metrics.roc_curve(correct, confs.max(axis=-1))
-
-    auc = sklearn.metrics.auc(fpr, tpr)
-    return auc
-
 def temper_probs(probs, T):
     # convert probs to logits
     logits = torch.log(probs)
@@ -94,3 +52,4 @@ def get_ood_detection_roc(ID_score, OOD_score):
     fpr, tpr, _ = sklearn.metrics.roc_curve(targets, score_vals)
     roc_auc = sklearn.metrics.auc(fpr, tpr)
     return fpr, tpr, roc_auc
+
