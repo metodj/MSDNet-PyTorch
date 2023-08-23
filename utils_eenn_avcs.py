f625b9fdd286fec6df87e648d91cc55873703050
metodj <jazbec.metod@gmail.com>
2023-08-23 14:56:14 +0200

    cifar10 experiments

--- utils_eenn_avcs.py
+++ utils_eenn_avcs.py
@@ -6,6 +6,9 @@ from utils import parse_args
 
 from collections import OrderedDict
 import random
+from typing import List, Union
+
+from laplace.lllaplace import FullLLLaplace
 
 
 def get_preds_per_exit(probs: torch.Tensor):
@@ -205,3 +208,129 @@ def consistency_classifciation(sets) -> float:
         else:
             cons_arr.append(size_intersect / size)
     return cons_arr
+
+
+
+class FullLLLaplaceBridge(FullLLLaplace):
+    def __call__(self, x, pred_type='glm', joint=False, link_approx='probit', 
+                 n_samples=100, diagonal_output=False, generator=None):
+        """Compute the posterior predictive on input data `x`.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            `(batch_size, input_shape)`
+
+        pred_type : {'glm', 'nn'}, default='glm'
+            type of posterior predictive, linearized GLM predictive or neural
+            network sampling predictive. The GLM predictive is consistent with
+            the curvature approximations used here.
+
+        link_approx : {'mc', 'probit', 'bridge', 'bridge_norm'}
+            how to approximate the classification link function for the `'glm'`.
+            For `pred_type='nn'`, only 'mc' is possible. 
+
+        joint : bool
+            Whether to output a joint predictive distribution in regression with
+            `pred_type='glm'`. If set to `True`, the predictive distribution
+            has the same form as GP posterior, i.e. N([f(x1), ...,f(xm)], Cov[f(x1), ..., f(xm)]).
+            If `False`, then only outputs the marginal predictive distribution. 
+            Only available for regression and GLM predictive.
+
+        n_samples : int
+            number of samples for `link_approx='mc'`.
+
+        diagonal_output : bool
+            whether to use a diagonalized posterior predictive on the outputs.
+            Only works for `pred_type='glm'` and `link_approx='mc'`.
+
+        generator : torch.Generator, optional
+            random number generator to control the samples (if sampling used).
+
+        Returns
+        -------
+        predictive: torch.Tensor or Tuple[torch.Tensor]
+            For `likelihood='classification'`, a torch.Tensor is returned with
+            a distribution over classes (similar to a Softmax).
+            For `likelihood='regression'`, a tuple of torch.Tensor is returned
+            with the mean and the predictive variance.
+            For `likelihood='regression'` and `joint=True`, a tuple of torch.Tensor 
+            is returned with the mean and the predictive covariance. 
+        """
+        if pred_type not in ['glm', 'nn']:
+            raise ValueError('Only glm and nn supported as prediction types.')
+
+        if link_approx not in ['mc', 'probit', 'bridge', 'bridge_norm']:
+            raise ValueError(f'Unsupported link approximation {link_approx}.')
+
+        if pred_type == 'nn' and link_approx != 'mc':
+            raise ValueError('Only mc link approximation is supported for nn prediction type.')
+        
+        if generator is not None:
+            if not isinstance(generator, torch.Generator) or generator.device != x.device:
+                raise ValueError('Invalid random generator (check type and device).')
+
+        if pred_type == 'glm':
+            f_mu, f_var = self._glm_predictive_distribution(
+                x,
+            )
+            # regression
+            if self.likelihood == 'regression':
+                return f_mu, f_var
+            # classification
+            if link_approx == 'mc':
+                return self.predictive_samples(x, pred_type='glm', n_samples=n_samples,).mean(dim=0)
+            elif link_approx == 'probit':
+                kappa = 1 / torch.sqrt(1. + np.pi / 8 * f_var.diagonal(dim1=1, dim2=2))
+                return torch.softmax(kappa * f_mu, dim=-1)
+            elif 'bridge' in link_approx:
+                # zero mean correction
+                f_mu -= (f_var.sum(-1) * f_mu.sum(-1).reshape(-1, 1) /
+                         f_var.sum(dim=(1, 2)).reshape(-1, 1))
+                f_var -= (torch.einsum('bi,bj->bij', f_var.sum(-1), f_var.sum(-2)) /
+                          f_var.sum(dim=(1, 2)).reshape(-1, 1, 1))
+                # Laplace Bridge
+                _, K = f_mu.size(0), f_mu.size(-1)
+                f_var_diag = torch.diagonal(f_var, dim1=1, dim2=2)
+                # optional: variance correction
+                if link_approx == 'bridge_norm':
+                    f_var_diag_mean = f_var_diag.mean(dim=1)
+                    f_var_diag_mean /= torch.as_tensor([K/2], device=self._device).sqrt()
+                    f_mu /= f_var_diag_mean.sqrt().unsqueeze(-1)
+                    f_var_diag /= f_var_diag_mean.unsqueeze(-1)
+                sum_exp = torch.exp(-f_mu).sum(dim=1).unsqueeze(-1)
+                alpha = (1 - 2/K + f_mu.exp() / K**2 * sum_exp) / f_var_diag
+                # return torch.nan_to_num(alpha / alpha.sum(dim=1).unsqueeze(-1), nan=1.0)
+                return alpha
+        else:
+            samples = self._nn_predictive_samples(x, n_samples)
+            if self.likelihood == 'regression':
+                return samples.mean(dim=0), samples.var(dim=0)
+            return samples.mean(dim=0)
+        
+
+def avcs_classification(
+    dirichlet_alphas: Union[np.array, torch.Tensor],
+    alpha: float = 0.05,
+    S: int = 1,
+    seed: int = 0,
+) -> List[List[int]]:
+    np.random.seed(seed)
+
+    L, C = dirichlet_alphas.shape
+    alpha_sums = dirichlet_alphas.sum(axis=1)
+
+    # Vectorized sampling
+    lik_samples = np.array(
+        [np.random.dirichlet(dirichlet_alphas[l]) for l in range(L) for _ in range(S)]
+    )
+    lik_samples = lik_samples.reshape(L, S, C)
+
+    R_denom = lik_samples.min(axis=1)
+    R_arr = (dirichlet_alphas / alpha_sums[:, None]) / R_denom
+
+    R_arr = R_arr.cumprod(axis=0)
+    C_arr = [list(np.where(row <= 1 / alpha)[0]) for row in R_arr]
+
+    C_arr = running_intersection_classification(C_arr)
+    return C_arr
